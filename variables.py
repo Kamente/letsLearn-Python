9d8cb0462e098598e13dcabc0fb11d0c232bb69f
kamente <justinm48000@gmail.com>
2026-02-13 11:44:25 +0300

    variables, data types

---
+++ variables.py
@@ -0,0 +1,15 @@
+_age = 23
+print(_age)
+
+print("Hello", "Dear")
+print('My favorite colors are', 'blue', 'green', 'red')
+
+print("Hello" ", " "Dear")
+
+## data types
+int age = 25
+string name = "Justin"
+float balance = 23.45
+boolean pass = True
+set: myMarks = {34, 20, 56, 78}
+Dictionary (key value pairs) myDict = {'name': "Justin", 'age': 24}

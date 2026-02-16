5014d214e7bd8fd4e4558a4289d3c058aea46616
kamente <justinm48000@gmail.com>
2026-02-16 12:11:38 +0300

    ints and float data types operations

---
+++ int_float.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+sum = a + b
+print(sum)
+print(type(sum))
+
+float1 = 54.6
+float2 = 45.3
+div = float1 / float2
+mod = float1 % float2  # modulus (returns reminder)
+floor_div = float1 // float2
+print(div, mod, floor_div)
--- strings.py
+++ strings.py
@@ -2,3 +2,36 @@ str1 = 'Justin'
 str2 = "Kim"
 print(f"Hello {str1} How is {str2} doing?")
 print("st" in str2)
+print(len(str2))
+
+str2_upper = str2.upper()
+print(str2_upper)
+
+str2_lower = str2.lower()
+print(str2_lower)
+
+# strip and replace
+# replace: string.replace("old", "new")
+str4 = "   Hello World   "
+str_strip = str4.strip()
+print(str_strip)
+
+str_replace = str_strip.replace("Hello", "Hi")
+print(str_replace)
+
+
+# startswith & endswith return boolean (true or false)
+str_startswith = str_strip.startswith("Hello")
+str_endswith = str_strip.endswith("People")
+print(str_startswith, str_endswith)
+
+
+str_count = str_strip.count('o')
+print(str_count)
+
+
+# isupper and islower (checks if all characters are upper or lower cases): returns boolean
+str5 = "how are you?"
+str_islower = str5.isupper()
+str_isupper = str5.islower()
+print(str_islower, str_isupper)

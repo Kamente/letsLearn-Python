eb1410fe144a99b888a6f33ff071d129322f3f68
kamente <justinm48000@gmail.com>
2026-02-18 11:48:12 +0300

    assign oper

--- int_float.py
+++ int_float.py
@@ -1,3 +1,5 @@
+# integers and float numbers
+
 a = 10
 b = 20
 sum = a + b
@@ -10,3 +12,22 @@ div = float1 / float2
 mod = float1 % float2  # modulus (returns reminder)
 floor_div = float1 // float2
 print(div, mod, floor_div)
+
+exponent_no = a**3
+print(exponent_no)
+
+convert_int = int(float1)
+print(convert_int)
+
+round_up = round(float2)
+print(round_up)
+
+
+# augmented assignment operators
+x = 20
+x -= 4
+print(x)
+
+x //= 3
+print(x)
+
